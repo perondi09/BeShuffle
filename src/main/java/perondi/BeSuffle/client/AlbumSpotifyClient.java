425710cc04c929ce42c5cd027bd168fafbfe4b94
perondi <guilhermeperondi09@gmail.com>
2026-03-13 19:17:59 -0300

    feat: create Interface album with openfeign

---
+++ src/main/java/perondi/BeSuffle/client/AlbumSpotifyClient.java
@@ -0,0 +1,16 @@
+package perondi.BeSuffle.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(
+        name = "AlbumSpotifyClient",
+        url = "https://api.spotify.com"
+)
+public interface AlbumSpotifyClient {
+
+    @GetMapping(value = "/v1/albums/{id}")
+    Album getAlbum(@RequestHeader("Authorization") String authorization, @PathVariable("id") String id);
+}
