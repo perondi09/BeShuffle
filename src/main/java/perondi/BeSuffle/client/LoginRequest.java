9b4ec7ae0b78e452ea1605f3f37725bb783c4492
perondi <guilhermeperondi09@gmail.com>
2026-03-13 19:16:56 -0300

    feat: create token verification

---
+++ src/main/java/perondi/BeSuffle/client/LoginRequest.java
@@ -0,0 +1,25 @@
+package perondi.BeSuffle.client;
+
+import feign.form.FormProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class LoginRequest {
+
+    @FormProperty("grant_type")
+    private String grantType;
+
+    @FormProperty("client_id")
+    private String clientId;
+
+    @FormProperty("client_secret")
+    private String clientSecret;
+
+}
---
+++ src/main/java/perondi/BeSuffle/client/LoginResponse.java
@@ -0,0 +1,20 @@
+package perondi.BeSuffle.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonNaming;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginResponse {
+
+    private String accessToken;
+
+}
